a991bf164c4967aeae8360b69204e18e0e98c6f4
foadyousefi <foadyousefi@gmail.com>
2023-04-15 13:49:28 +0200

    Remove styling from main route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,41 +29,5 @@
 </section>
 
 <style lang="scss">
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
 
-		p {
-			margin: 0;
-		}
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	h2 {
-		strong {
-			color: tomato;
-		}
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
