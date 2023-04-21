47d19a38b5930fb6fa676bbfd431eb09bd315826
foadyousefi <foadyousefi@gmail.com>
2023-04-22 01:05:43 +0200

    Set resulting images

---
+++ src/lib/ResultNavnelapp.svelte
@@ -0,0 +1,88 @@
+<script>
+  import { icons, selectedIcon } from '$lib/stores/icons';
+  import { backgrounds, selectedBackground } from '$lib/stores/backgrounds';
+  import { fonts, selectedFont, colors, selectedColor } from '$lib/stores/fonts';
+  export let showIcon
+  export let firstInput
+  export let secondInput
+  export let thirdInput
+  export let firstFont
+  export let secondFont
+  export let thirdFont
+
+</script>
+
+<div class="result-image" style="font-family: {$selectedFont.family};color:{$selectedColor}">
+  {#if $selectedBackground}
+    {#if $selectedBackground.category === 'solidColor'}
+      <svg class="background-image" width="300" height="130" viewBox="0 0 300 130" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="300" height="130" fill={$selectedBackground.value}/>
+      </svg>
+    {:else}
+      <img class="background-image" src={$selectedBackground.value} alt={$selectedBackground.name}>
+    {/if}
+  {/if}
+  {#if showIcon && $selectedIcon}
+    <img src={$selectedIcon.path} alt={$selectedIcon.name}>
+  {/if}
+  <div class="text">
+    {#if firstInput.length}
+      <div class="first" style="font-size: {firstFont}px;">{firstInput}</div>
+    {/if}
+    {#if secondInput.length}
+      <div class="second" style="font-size: {secondFont}px;">{secondInput}</div>
+    {/if}
+    {#if thirdInput.length}
+      <div class="third" style="font-size: {thirdFont}px;">{thirdInput}</div>
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+
+  .result-image {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background: tomato;
+    border-radius: 14px;
+    aspect-ratio: 30/13;
+    position: relative;
+    isolation: isolate;
+
+    .text {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+      position: relative;
+      top: -2px;
+      height: 100%;
+
+      > div {
+        line-height: 0.5;
+        font-weight: 700;
+        z-index: 1;
+      }
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      z-index: 1;
+    }
+
+    .background-image {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 14px;
+      z-index: 0;
+    }
+  }
+</style>
--- src/routes/navnelapper/+page.svelte
+++ src/routes/navnelapper/+page.svelte
@@ -2,6 +2,7 @@
   import type { PageData } from './$types';
   import { onMount } from 'svelte';
   import navnelapp from '$lib/stores/navnelapp';
+  import ResultNavnelapp from '$lib/ResultNavnelapp.svelte';
   import { icons, selectedIcon } from '$lib/stores/icons';
   import { backgrounds, selectedBackground } from '$lib/stores/backgrounds';
   import { fonts, selectedFont, colors, selectedColor } from '$lib/stores/fonts';
@@ -77,31 +78,7 @@
         </div>
       </div>
       <div class="preview">
-        <div class="result-image" style="font-family: {$selectedFont.family};color:{$selectedColor}">
-          {#if $selectedBackground}
-            {#if $selectedBackground.category === 'solidColor'}
-              <svg class="background-image" width="300" height="130" viewBox="0 0 300 130" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="300" height="130" fill={$selectedBackground.value}/>
-              </svg>
-            {:else}
-              <img class="background-image" src={$selectedBackground.value} alt={$selectedBackground.name}>
-            {/if}
-          {/if}
-          {#if showIcon && $selectedIcon}
-            <img src={$selectedIcon.path} alt={$selectedIcon.name}>
-          {/if}
-          <div class="text">
-            {#if firstInput.length}
-              <div class="first" style="font-size: {firstFont}px;">{firstInput}</div>
-            {/if}
-            {#if secondInput.length}
-              <div class="second" style="font-size: {secondFont}px;">{secondInput}</div>
-            {/if}
-            {#if thirdInput.length}
-              <div class="third" style="font-size: {thirdFont}px;">{thirdInput}</div>
-            {/if}
-          </div>
-        </div>
+        <ResultNavnelapp {showIcon} {firstInput} {secondInput} {thirdInput} {firstFont} {secondFont} {thirdFont} />
       </div>
     </div>
     <div class="order-actions">
@@ -187,31 +164,7 @@
           <h2>Velg en font</h2>
           <div class="fonts">
             {#each $fonts as font}
-              <div class="result-image {$selectedFont.id === font.id ? 'selected' : ''}" style="font-family:{font.family};color:{$selectedColor}" on:click={() => selectFont(font)}>
-                {#if $selectedBackground}
-                  {#if $selectedBackground.category === 'solidColor'}
-                    <svg class="background-image" width="300" height="130" viewBox="0 0 300 130" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <rect width="300" height="130" fill={$selectedBackground.value}/>
-                    </svg>
-                  {:else}
-                    <img class="background-image" src={$selectedBackground.value} alt={$selectedBackground.name}>
-                  {/if}
-                {/if}
-                {#if showIcon && $selectedIcon}
-                  <img src={$selectedIcon.path} alt={$selectedIcon.name}>
-                {/if}
-                <div class="text">
-                  {#if firstInput.length}
-                    <div class="first" style="font-size: {firstFont / 1.2}px;">{firstInput}</div>
-                  {/if}
-                  {#if secondInput.length}
-                    <div class="second" style="font-size: {secondFont / 1.2}px;">{secondInput}</div>
-                  {/if}
-                  {#if thirdInput.length}
-                    <div class="third" style="font-size: {thirdFont / 1.2}px;">{thirdInput}</div>
-                  {/if}
-                </div>
-              </div>
+              <ResultNavnelapp {showIcon} {firstInput} {secondInput} {thirdInput} {firstFont} {secondFont} {thirdFont} on:click={() => selectedFont(font)} />
             {/each}
           </div>
           <h2>Velg tekst farge</h2>
@@ -281,51 +234,6 @@
     justify-content: center;
   }
 
-  .result-image {
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background: tomato;
-    border-radius: 14px;
-    aspect-ratio: 30/13;
-    position: relative;
-    isolation: isolate;
-
-    .text {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: space-evenly;
-      position: relative;
-      top: -2px;
-      height: 100%;
-
-      > div {
-        line-height: 0.5;
-        font-weight: 700;
-        z-index: 1;
-      }
-    }
-
-    img {
-      width: 100%;
-      height: 100%;
-      z-index: 1;
-    }
-
-    .background-image {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      border-radius: 14px;
-      z-index: 0;
-    }
-  }
 
   .modifiers {
     display: flex;
