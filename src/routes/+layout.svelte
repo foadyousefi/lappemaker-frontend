732ef073efddd14937d5125da353b5c6d2ec7f77
foadyousefi <foadyousefi@gmail.com>
2023-04-27 19:44:48 +0200

    Add route fade transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
 <script>
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
+	import { fade } from "svelte/transition";
 	import './styles.css';
+
+	export let data
 </script>
 
 <div class="app">
 	<Header />
-
-	<main>
+	{#key data.pathname}
+	<main
+				in:fade={{ duration: 200, delay: 200 }}
+				out:fade={{ duration: 200 }}
+	>
 		<slot />
 	</main>
+		{/key}
 
 	<Footer />
 </div>
