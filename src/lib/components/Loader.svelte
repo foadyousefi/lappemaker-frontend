de385c3d1ecc8d24684423538dfbdbe0612706f7
foadyousefi <foadyousefi@gmail.com>
2023-04-30 18:53:19 +0200

    create Loader component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,17 @@
+<div class="loader">
+  <h1>Loading</h1>
+</div>
+
+<style lang="scss">
+  .loader {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 10;
+  }
+</style>
