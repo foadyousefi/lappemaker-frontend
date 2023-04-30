1e557719e9e5b6504f1953a40aebdc2a63ff33ac
foadyousefi <foadyousefi@gmail.com>
2023-04-30 18:55:21 +0200

    Add route transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,32 @@
 <script>
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
-	import { fade } from "svelte/transition";
+	import { fly } from "svelte/transition";
+	import { cubicIn, cubicOut } from 'svelte/easing'
+	import Loader from '$lib/components/Loader.svelte';
+	import { beforeNavigate, afterNavigate } from '$app/navigation'
+
 	import './styles.css';
 
+	const duration = 300;
+	const delay = 400;
+	const x = 100;
 	export let data
+	let isLoading = false
+
+	beforeNavigate(() => isLoading = true)
+	afterNavigate(() => isLoading = false)
 </script>
 
 <div class="app">
 	<Header />
+	{#if isLoading}
+		<Loader />
+	{/if}
 	{#key data.pathname}
 	<main
-				in:fade={{ duration: 200, delay: 200 }}
-				out:fade={{ duration: 200 }}
+				in:fly={{ easing: cubicOut, duration, delay, x }}
+				out:fly={{ easing: cubicIn, duration, x: -x }}
 	>
 		<slot />
 	</main>
