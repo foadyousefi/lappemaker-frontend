cfb2b8fe2bfe44b99deb88f8dd46eb0a8999fe61
foadyousefi <foadyousefi@gmail.com>
2023-04-30 20:36:02 +0200

    Change loader

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,17 +1,46 @@
-<div class="loader">
-  <h1>Loading</h1>
-</div>
+<div class="loader"></div>
 
 <style lang="scss">
   .loader {
     position: fixed;
+    z-index: 999;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 5px;
+    background-color: transparent;
     display: flex;
     justify-content: center;
     align-items: center;
-    left: 0;
-    top: 0;
-    width: 100vw;
-    height: 100vh;
-    z-index: 10;
+
+    &::after {
+      content: '';
+      left: 0;
+      width: 100%;
+      height: 10px;
+      background-color: tomato;
+      animation: loader 1s linear infinite;
+      transform: scaleX(0);
+      transform-origin: left;
+    }
+  }
+
+  @keyframes loader {
+    0% {
+      transform-origin: left;
+      transform: scaleX(0);
+    }
+    49% {
+      transform-origin: left;
+      transform: scaleX(1);
+    }
+    55% {
+      transform-origin: right;
+      transform: scaleX(1);
+    }
+    100% {
+      transform-origin: right;
+      transform: scaleX(0);
+    }
   }
 </style>
