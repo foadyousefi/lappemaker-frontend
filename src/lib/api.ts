cac2b01b85fb37aa905914c223e52c0fb3d6eabf
foadyousefi <foadyousefi@gmail.com>
2023-04-30 18:51:54 +0200

    Create PUT api function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,10 @@
 import { BASEURL, CLIENTKEY, CLIENTSECRET } from '$env/static/private';
 
 export async function get( resource ) {
-    return await fetch(`${BASEURL}/${resource}`, {
+    return await fetch(`${BASEURL}/products/${resource}`, {
         method: 'GET',
         headers: {
-            'Authorization': `Basic ${btoa(`${CLIENTKEY}:${CLIENTSECRET}`)}`
+            'Authorization': `Basic ${Buffer.from(`${CLIENTKEY}:${CLIENTSECRET}`, 'utf-8').toString('base64')}`
         }
     });
 }
@@ -14,7 +14,18 @@ export async function post(resource, data) {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
-            'Authorization': `Basic ${btoa(`${CLIENTKEY}:${CLIENTSECRET}`)}`
+            'Authorization': `Basic ${Buffer.from(`${CLIENTKEY}:${CLIENTSECRET}`, 'utf-8').toString('base64')}`
+        },
+        body: data && JSON.stringify(data)
+    });
+}
+
+export async function put(resource, data) {
+    return fetch(`${BASEURL}/${resource}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `Basic ${Buffer.from(`${CLIENTKEY}:${CLIENTSECRET}`, 'utf-8').toString('base64')}`
         },
         body: data && JSON.stringify(data)
     });
