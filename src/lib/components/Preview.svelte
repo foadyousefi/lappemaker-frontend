a68f102d00712d2ecbed441c8039526e2904a75f
foadyousefi <foadyousefi@gmail.com>
2023-07-03 01:00:52 +0200

    Create component for preview

---
+++ src/lib/components/Preview.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from 'svelte';
+    import navnelapp from '$lib/stores/navnelapp';
+    import CanvasNavnelapp from '$lib/CanvasNavnelapp.svelte';
+    import { selectedFont } from '$lib/stores/fonts';
+
+    let firstInput = ''
+    let secondInput = ''
+    let thirdInput = ''
+    let showIcon = true
+
+    onMount(() => {
+      firstInput = $navnelapp.firstInput
+      secondInput = $navnelapp.secondInput
+      thirdInput = $navnelapp.thirdInput
+      showIcon = $navnelapp.showIcon
+  })
+</script>
+
+
+<CanvasNavnelapp {showIcon} {firstInput} {secondInput} {thirdInput} index="top" fontFamily={$selectedFont} />
